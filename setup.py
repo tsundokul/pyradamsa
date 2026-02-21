fbcb545e26ad109dc818ec157331de423dd8e89d
Minttu <minttu.tuulensuu@protonmail.com>
2026-02-21 09:10:56 +0200

    Migrate build tooling to pyproject.toml and uv (#5)
    -  Replace requirements.txt and pytest.ini with pyproject.toml.
    -  Simplify setup.py to only define the C extension.
    -  Conditionally link -lrt on Linux for cross-platform support.
    -  Update build instructions and run_tests to use uv.
    -  Bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -1,40 +1,23 @@
+#!/usr/bin/env python3
+import sys
+from setuptools import setup, Extension
 
-from setuptools import setup, Extension, find_packages
-from pyradamsa.version import __version__
+extra_link_args = []
+if sys.platform == 'linux':
+    extra_link_args.append('-lrt')
 
-libradamsa = Extension('libradamsa',
-   sources=['libradamsa/libradamsa.c'],
-   include_dirs=['libradamsa'],
-   py_limited_api=True,
-   extra_compile_args=['-O3', '-lrt']
+libradamsa = Extension(
+    "libradamsa",
+    sources=["libradamsa/libradamsa.c"],
+    include_dirs=["libradamsa"],
+    py_limited_api=True,
+    extra_compile_args=["-O3"],
+    extra_link_args=extra_link_args,
 )
 
-setup(name='pyradamsa',
-      version=__version__,
-      description='Python bindings for radamsa fuzzing library.',
-      long_description_content_type='text/markdown',
-      long_description=open('README.md').read().strip(),
-      author='Daniel Timofte @tim17d',
-      author_email='timofte.daniel@tuta.io',
-      url='https://github.com/tsundokul/pyradamsa',
-      py_modules=['pyradamsa'],
-      install_requires=[],
-      license='MIT License',
-      zip_safe=False,
-      keywords='radamsa fuzzing libradamsa',
-      packages=['pyradamsa'],
-      include_package_data=True,
-      setup_requires=["wheel"],
-      classifiers=[
-         "Development Status :: 4 - Beta",
-         "Topic :: Security",
-         "Operating System :: POSIX :: Linux",
-         "License :: OSI Approved :: MIT License",
-         'Programming Language :: Python :: 3',
-      ],
-      ext_modules=[libradamsa],
-      options={
-          'bdist_wheel': {'python_tag': 'cp30', 'py_limited_api': 'cp32'},
-          'build_ext': {'build_lib': 'pyradamsa/lib'}
-      }
-   )
+setup(
+    ext_modules=[libradamsa],
+    options={
+        'build_ext': {'build_lib': 'pyradamsa/lib'}
+    }
+)
